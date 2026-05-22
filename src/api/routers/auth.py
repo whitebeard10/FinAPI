cc0b63f6c8758c6f0f9453d8c47cbe0eec7e9f70
Avinash Chhetri <103208764+whitebeard10@users.noreply.github.com>
2026-05-22 17:20:18 +0530

    fix: explicit annotated dependencies for login to solve swagger 422

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from src.api.schemas.auth import UserCreate, UserResponse, Token, LoginRequest, TokenRefreshRequest
@@ -12,9 +13,13 @@ async def register(user_in: UserCreate, db: DBDep):
     return await auth_service.register(user_in)
 
 @router.post("/login", response_model=Token)
-async def login(db: DBDep, form_data: OAuth2PasswordRequestForm = Depends()):
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: DBDep
+):
     """
     Standard OAuth2 compatible login for Swagger UI.
+    Use 'username' field for email.
     """
     auth_service = AuthService(db)
     return await auth_service.login(LoginRequest(email=form_data.username, password=form_data.password))
